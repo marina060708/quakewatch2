aeb9f173f0995438c9970fdc873bf938e0ea78e0
marina060708 <marina.kehat@gmail.com>
2025-10-27 08:02:41 +0200

    Add metrics for Prometheus

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+# Initialize Flask app
 app = Flask(__name__)
 
+# Attach Prometheus metrics exporter
+metrics = PrometheusMetrics(app)
+
+# Define your routes
 @app.route("/")
 def home():
     return "Hello from QuakeWatch2!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
-from prometheus_flask_exporter import PrometheusMetrics
-metrics = PrometheusMetrics(app)
+    app.run(host="0.0.0.0", port=8000)
